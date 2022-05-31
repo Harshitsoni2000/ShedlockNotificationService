3d73fe3762710182a5a160d18ac54a27dd5bfc5f
Harshitsoni2000 <harshitsoni.ajmer23@gmail.com>
2022-05-31 14:22:56 +0530

    Added Shedlock

--- src/main/java/com/example/shedlockmanager/shedlock/config/SchedulerConfig.java
+++ src/main/java/com/example/shedlockmanager/shedlock/config/SchedulerConfig.java
@@ -2,8 +2,6 @@ package com.example.shedlockmanager.shedlock.config;
 
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,9 +9,6 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SchedulerConfig {
-
-    @Autowired
-    private DataSourceConfig dataSourceConfig;
 
     @Bean
     public LockProvider lockProvider(DataSource dataSource) {
